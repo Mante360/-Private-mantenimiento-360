e44570e618bc43fa289566e4631d67baf06a1999
Mante360 <thonyfa27@gmail.com>
2026-09-07 21:51:07 -0300

    Fix money formatting in app.js

--- app.js
+++ app.js
@@ -491,7 +491,7 @@ const savedRating = JSON.parse(localStorage.getItem('professionalRating') || 'nu
       <p><b>Servicio:</b> ${quote?.specialty || state.job.service}</p>
       <p><b>Profesional:</b> ${state.job.professional}</p>
       <p><b>Localidad:</b> ${quote?.location || state.job.locality}</p>
-      <div class="money">$${money(quote?.amount || 0)}</div>
+      <div class="money">${money(quote?.amount || 0)}</div>
       <p>${quote?.detail || 'Presupuesto enviado por el profesional.'}</p>
     </div>
 
